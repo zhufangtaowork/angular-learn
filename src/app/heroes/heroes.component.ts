2779ed8b2154948214195707131134bae2b538a9
zhufangtao <zhufangtao_work@sina.com>
2020-12-29 09:56:23 +0800

    首次提交

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,9 +9,9 @@ import {MessageService} from '../service/message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
+  heroes: Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
